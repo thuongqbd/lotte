63b72d02dabd5e78f0132c44817e1e46410208cf
rockchin <hoang_duyen2367@yahoo.com.vn>
2015-06-02 08:19:31 +0700

    add link fb in footer fix size slider in header add new db/happytou_lotte_262015.sql

--- wp-content/themes/lottehappytour/inc/custom-header.php
+++ wp-content/themes/lottehappytour/inc/custom-header.php
@@ -26,8 +26,8 @@ function lottehappytour_custom_header_setup() {
 		'default-image'          => '',
 
 		// Set height and width, with a maximum value for the width.
-		'height'                 => 511,
-		'width'                  => 1170,
+		'height'                 => 420,
+		'width'                  => 960,
 		'max-width'              => 2000,
 
 		// Support flexible height and width.
