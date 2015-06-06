c7259ea049c41c8c0f80f77e9f397061053d0798
trung <ttrung.david@gmail.com>
2015-06-06 23:33:56 +0700

    init page route

---
+++ wp-content/themes/lottehappytour/page-templates/route-destination.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Template Name: ROUTE DESTINATION
+ *
+ * Description: A page template that provides a key component of WordPress as a CMS
+ * by meeting the need for a carefully crafted introductory page. The front page template
+ * in Twenty Twelve consists of a page content area for adding text, images, video --
+ * anything you'd like -- followed by front-page-only widgets in one or two columns.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twelve
+ * @since Twenty Twelve 1.0
+ */
+get_header(); 
+global $post;
+?>
+<link href="<?= get_template_directory_uri()?>/css/route.css" rel="stylesheet" type="text/css">
+	<section id="primary" class="site-content">
+		<div id="content" role="main">
+			<div class="container route">
+			<?php while ( have_posts() ) : the_post(); ?>
+					<div class="title-home happy-diary-title">
+						<table cellspacing="0" cellpadding="0">
+							<tbody>
+								<tr>
+									<td class="first"><h2><?php the_title(); ?></h2></td>
+									<td class="second"><img src="<?= get_template_directory_uri()?>/images/breadcrumb-arrow.png" alt=""></td>
+									<td class="line-title">&nbsp;</td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+					<div class="route-container">
+					<?php the_content(); ?>
+					</div>
+			<?php endwhile; // end of the loop. ?>
+			</div>
+		</div>
+	</section>
+<?php get_sidebar( 'front' ); ?>
+<?php get_footer(); ?>
